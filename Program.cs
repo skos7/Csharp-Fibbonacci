dc956e68ab9b106f6693a72c4d25c94bddb5c9f6
Sandi Kos <sandi.7kos@gmail.com>
2022-09-06 17:37:49 +0200

    Csharp-Fibbonacci sequence

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+
+        static void fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int next = a + b;
+
+            Console.WriteLine(a + "\n" + b);
+
+            for(int i = 3; i < n; i++)
+            {
+                Console.WriteLine(next);
+                a = b;
+                b = next;
+                next = a + b;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Insert Fibonacci number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+            fibonacci(n);
+        }
+    }
+}
